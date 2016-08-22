3de0a6fe829a87449a7259548b8f77b4618ce634
shuchun <shuchuneric@163.com>
2016-08-22 09:48:08 +0800

    spring boot social

---
+++ auth/src/main/java/com/example/oauth/controller/OAuthController.java
@@ -0,0 +1,13 @@
+package com.example.oauth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by IBM on 2016/8/19.
+ * oauth 2.0 controller
+ */
+@Controller
+@RequestMapping("/oauth")
+public class OAuthController {
+}
