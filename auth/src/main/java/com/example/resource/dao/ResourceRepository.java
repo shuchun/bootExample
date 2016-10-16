299ac28ccbec7bad51927f09bb82be16be933992
shuchun <shuchuneric@163.com>
2016-10-16 23:55:17 +0800

    update

--- auth/src/main/java/com/example/resource/dao/ResourceRepository.java
+++ auth/src/main/java/com/example/resource/dao/ResourceRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Set;
 
 /**
  * Created by IBM on 2016/9/9.
